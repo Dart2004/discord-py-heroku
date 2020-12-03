db2657107a4922db05535bab1041b0256fc88788
Dart2.0 <59048496+Dart2004@users.noreply.github.com>
2020-12-03 08:51:46 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -645,9 +645,9 @@ async def on_message(msg):
 		if str(emoji) in msg.content:
 		  names.append(emoji.name)
 		  continue
-                msg.content=msg.content.replace("<:","abgtegt")
+		msg.content=msg.content.replace("<:","abgtegt")
 		msg.content = msg.content.replace(":" + str(emoji.name.lower()) +":",str(emoji))
-                msg.content=msg.content.replace("abgtegt","<:")
+		msg.content=msg.content.replace("abgtegt","<:")
 		names.append(emoji.name)
 	print(message, msg.content)
 	if msg.content == message:
