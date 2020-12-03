1d4edb19eff06310ad89f1995b10801edcfcb5a9
Dart2.0 <59048496+Dart2004@users.noreply.github.com>
2020-12-03 10:10:43 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -282,7 +282,7 @@ async def kick(ctx, *, user):
 	  user=int(user.replace("<","").replace("@","").replace("!","").replace(">",""))
 	  member=client.get_user(user)
 	  await member.send("You got kicked in " + ctx.guild.name)
-	  await guild.kick(user)
+	  await ctx.guild.kick(user)
 	else:
 		await ctx.send("Only admin")
 
@@ -293,7 +293,7 @@ async def ban(ctx, *, username):
 		user=int(user.replace("<","").replace("@","").replace("!","").replace(">",""))
 		member=client.get_user(user)
 		await member.send("You got banned in " + ctx.guild.name)
-		await guild.ban(user)
+		await ctx.guild.ban(user)
 	else:
 		await ctx.send("Only admin")
 
